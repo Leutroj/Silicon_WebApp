e1148d92afa190c4075d221286e5ab5c9fefa1dc
Alexander Leu <leutroj@gmail.com>
2024-04-20 16:27:25 +0200

    Entities

---
+++ Infrastructure/Contexts/AddressEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Infrastructure.Contexts;
+
+public class AddressEntity
+{
+    public int Id { get; set; } 
+    ´public string AdressLine_1 { get; set; } = null!;
+    public string AdressLine_2 { get; set; } = null!;
+    public string PostalCode { get; set; } = null!;
+
+    public string City { get; set; } = null!;
